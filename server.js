3979305e720df13afbb83799d24b74a8ff6f78be
Sunaa <sunaa@sunaarisu.de>
2023-08-03 16:20:32 +0200

    aoeu

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(5001, () => console.log('Api running on port 5001'));
+const port = process.env.PORT || 3000;
+
+app.listen(5001, "0.0.0.0", () => console.log('Api running on port 5001'));
 
 app.get('/', (req, res) => res.json('My api running'));
